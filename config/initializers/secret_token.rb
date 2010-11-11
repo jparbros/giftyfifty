5e53695ae60b13e86a91caa393249f20acb3d11e
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-11-11 01:52:47 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftyfifty::Application.config.secret_token = 'd2493a9bd423b35563ab80b0d4e9c148be53d7d0216f748d2a7b375cd27c1e91d26758b765d6fb0c0f3a512025d80b0fd3d13a3343fe84e498da893b1538d9d6'
