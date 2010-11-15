06ecc3a49c89fe13bd3eb86ae7f90f6366ff0376
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-11-14 20:56:59 -0600

    Front page, if you arent login, sign in box show

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,24 @@
 module MainHelper
+  def form_path(type)
+    case type
+    when 'sign-in'
+      registration_path(@user)
+    when 'login'
+      session_path(@user)
+    end
+  end
+  
+  def button_label(type)
+    case type
+    when 'sign-in'
+      'Sign Up'
+    when 'login'
+      'Login'
+    end
+  end
+  
+  def submit_search
+    submit_tag 'Next', {:id => (user_signed_in?)? 'search-gift': 'go-sign-in'}
+  end
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,52 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var loginBox, signinBox, loginLink;
+
+// Caching most used elements of the dom
+var cacheElements = function() {
+  loginBox = $('div#login-box');
+  signinBox = $("div#sign-in-box");
+  loginLink = $('a#login-link');
+  buttonLogin = $('input#go-sign-in');
+  buttonSearch = $('input#search-gift');
+};
+
+// Initializing boxes to login and sign in
+var setBoxes = function() {
+  loginBox.dialog({
+    resizable: false,
+    stack: false,
+    autoOpen: false,
+    height: 215,
+    width: 650,
+    modal: true,
+  });
+  
+  signinBox.dialog({
+    resizable: false,
+    stack: false,
+    autoOpen: false,
+    height: 305,
+    width: 650,
+    modal: true,
+  });
+};
+
+// Declaring the events
+var declaringEvents = function() {
+  loginLink.click(function() {
+    signinBox.dialog('close');
+    loginBox.dialog('open');
+    return false;
+  });
+  buttonLogin.click(function() {
+    signinBox.dialog('open');
+  });
+};
+
+$(document).ready(function() {
+  cacheElements();
+  setBoxes();
+  declaringEvents();
+});
