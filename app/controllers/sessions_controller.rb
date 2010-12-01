ce6781d6563fa17a88cf8a0ed43e1df1164f1aa9
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-01 13:19:20 -0600

    minor change to create account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     
     user = User.find_by_uid_provider(auth['uid'])
-    unless @user.blank?
+    if user.blank?
+      user = User.create_by_provider(auth,params[:provider])
       sign_in_and_redirect(:user, user)
     else
-      user = User.create_by_provider(auth,params[:provider])
       sign_in_and_redirect(:user, user)
     end
   end
