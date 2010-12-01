c63ab275f895421297c1037ca191f7124c5b4f5e
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-01 16:13:25 -0600

    Share on twitter, fix

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,6 +1,6 @@
 class TwittersController < ApplicationController
   def create
-    event = current_user.events.where(:id => params[:event_id])
+    event = current_user.events.where(:id => params[:id]).first
     event.share_on_twitter
     redirect_to user_event_path(current_user, event)
   end
