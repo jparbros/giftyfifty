d100d01cbd2edbca7c54183a0e4a46088c9949ee
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-02 10:09:30 -0600

    Fixed a routes problems

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,12 +8,12 @@ module EventsHelper
   end
   
   def twitter_link
-    link_path = (current_user.twitter_account)?  user_event_twitter_path(current_user, @event) : new_my_account_twitter_path
+    link_path = (current_user.twitter_account)?  new_user_event_twitter_path(current_user, @event) : new_my_account_twitter_path
     link_to(image_tag('twitter_64.png'), link_path)
   end
   
   def facebook_link
-    link_path = (current_user.facebook_account)?  user_event_facebook_path(current_user, @event) : new_my_account_facebook_path
+    link_path = (current_user.facebook_account)?  new_user_event_facebook_path(current_user, @event) : new_my_account_facebook_path
     link_to(image_tag('facebook_64.png'), link_path)
   end
 end
