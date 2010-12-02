bba48249c21e2e8a81f62cada0e96442c71f2c0a
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-02 10:25:08 -0600

    Fixed a typo

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -1,7 +1,7 @@
 class FacebooksController < ApplicationController
   def create
     event = current_user.events.where(:id => params[:event_id]).first
-    event.share_on_facebook(user_event_url(current_user, eventg))
+    event.share_on_facebook(user_event_url(current_user, event))
     redirect_to user_event_path(current_user, event)
   end
 end
