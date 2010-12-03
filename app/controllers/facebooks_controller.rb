d6d3990e778bf94478c827764ea330a82feba966
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-03 13:51:54 -0600

    Changed the route

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -1,7 +1,7 @@
 class FacebooksController < ApplicationController
   def create
     event = current_user.events.where(:id => params[:event_id]).first
-    event.share_on_facebook(user_event_url(current_user, event),params[:message])
+    event.share_on_facebook(event_url(current_user, event),params[:message])
     redirect_to event_path(current_user, event)
   end
 end
