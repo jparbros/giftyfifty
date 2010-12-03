ef21d602d4fc5f46df56fb6a683bc981364669ff
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-03 13:53:34 -0600

    Removed the curent_user from the path

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -1,7 +1,7 @@
 class FacebooksController < ApplicationController
   def create
     event = current_user.events.where(:id => params[:event_id]).first
-    event.share_on_facebook(event_url(current_user, event),params[:message])
+    event.share_on_facebook(event_url(event),params[:message])
     redirect_to event_path(current_user, event)
   end
 end
