157d3bef4b181a7b09c82d178cd681281d6c85f8
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-10 02:42:44 -0600

    fixed error login with social account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, TWITTER_CREDENTIALS['consumer_key'], TWITTER_CREDENTIALS['consumer_secret']
-  provider :facebook, FACEBOOK_CREDENTIAL['client_id'], FACEBOOK_CREDENTIAL['secret_id']
+  provider :twitter, TWITTER_CREDENTIALS[:consumer_key], TWITTER_CREDENTIALS[:consumer_secret]
+  provider :facebook, FACEBOOK_CREDENTIAL[:client_id], FACEBOOK_CREDENTIAL[:secret_id]
 end
