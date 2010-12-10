3b8a4dd307b2ad9fc5ecdca5949b1ebae2028de7
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-10 13:52:53 -0600

    supported kindle page for amazon

--- lib/scraps/amazon.rb
+++ lib/scraps/amazon.rb
@@ -5,32 +5,70 @@ module Scrap
     attr_accessor :hp
     
     def initialize(url)
+      @url = url
       @hp = Hpricot(open(url))
     end
     
     def price
-      (@hp/"tr>td>b.priceLarge").inner_text.gsub('$','').to_f
+      (@hp.search "tr>td>b.priceLarge").inner_text.gsub('$','').to_f
     end
     
     def image
-      (@hp/"#prodImageCell>a >img").attr('src')
+      normal_image || kindle_image
     end
     
     def name
-      (@hp/"h1.parseasinTitle > span#btAsinTitle").inner_text
+      (@hp.search "h1.parseasinTitle > span#btAsinTitle").inner_text
     end
     
     def asin
-      (@hp/"td.bucket > .content > ul > li").select {|t| t.inner_text.include?'ASIN'}.first.inner_text.gsub('ASIN: ','')
+      normal_asin || kindle_asin
     end
     
     def category
-      (@hp/".navCatSpc >.navCatA").inner_text
+      (@hp.search ".navCatSpc >.navCatA").inner_text
     end
     
     def weight
-      element = (@hp/"td.bucket > .content > ul > li").select {|t| t.inner_text.include?'Shipping Weight'}.first
-      element.inner_text.gsub(/\D/,'').to_i if element
+      normal_weight || kindle_weight
     end
+    
+    private
+     def normal_image
+       image = (@hp.search "#prodImageCell>a >img")
+       unless image.blank?
+         image.attr('src')
+       else
+         nil
+       end
+     end
+     
+     def kindle_image
+       image = (@hp.search ".at-a-glance-outer>tbody>tr>td>div #kindle-at-a-glance-image")
+       unless image.blank?
+         image.attr('src')
+       else
+         nil
+       end
+     end
+     
+     def normal_weight
+       element = (@hp.search "td.bucket > .content > ul > li").select {|t| t.inner_text.include?'Shipping Weight'}.first
+       element.inner_text.gsub(/\D/,'').to_i unless element.blank?
+     end
+     
+     def kindle_weight
+       element = ((@hp.search '#technical-details > table')[1].search 'tr').select {|t| t.inner_text.include?'Weight'}.first
+       element.inner_text.gsub(/ounces.*$/,'').gsub('Weight','').to_f
+     end
+     
+     def normal_asin
+        element = (@hp.search "td.bucket > .content > ul > li")
+        (@hp.search "td.bucket > .content > ul > li").select {|t| t.inner_text.include?'ASIN'}.first.inner_text.gsub('ASIN: ','') unless element.blank?
+     end
+     
+     def kindle_asin
+        @url.split('/').select {|t| t.size == 10}.first
+     end
   end
 end
--- lib/scraps/ebay.rb
+++ lib/scraps/ebay.rb
@@ -9,27 +9,27 @@ module Scrap
     end
     
     def price
-      (@hp/".vi-is1-solid.vi-is1-tbll>span").inner_text.gsub(/^.*\$/,'').to_f
+      (@hp.search ".vi-is1-solid.vi-is1-tbll>span").inner_text.gsub(/^.*\$/,'').to_f
     end
     
     def image
-      (@hp/".ic-m> center > img").attr('src')
+      (@hp.search ".ic-m> center > img").attr('src')
     end
     
     def name
-      (@hp/".vi-is1-titleH1").inner_text
+      (@hp.search ".vi-is1-titleH1").inner_text
     end
     
     def asin
-      (@hp/".z_b>.sp1>tr>td")[1].inner_text
+      (@hp.search ".z_b>.sp1>tr>td")[1].inner_text
     end
     
     def shipping
-      (@hp/".vi-is1-clr > #fshippingCost").inner_text
+      (@hp.search ".vi-is1-clr > #fshippingCost").inner_text
     end
     
     def category
-      (@hp/".in>li>a").last.inner_text
+      (@hp.search ".in>li>a").last.inner_text
     end
   end
 end
