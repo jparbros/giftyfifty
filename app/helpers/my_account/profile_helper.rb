6a7bc49699d371722f73802a519e19cc2ede678b
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-19 20:17:50 -0600

    Added sidebar to link social accounts and menu is completed

---
+++ app/helpers/my_account/profile_helper.rb
@@ -0,0 +1,17 @@
+module MyAccount::ProfileHelper
+  def add_twitter
+    if current_user.twitter_account
+      image_tag('connect_twitter.png', :class => 'connected_class')
+    else
+      link_to(image_tag('connect_twitter.png'), new_my_account_twitter_path, :class => 'connect_class')
+    end
+  end
+  
+  def add_facebook
+    if current_user.facebook_account
+      image_tag('connect_facebook.png', :class => 'connected_class')
+    else
+      link_to(image_tag('connect_facebook.png'), new_my_account_facebook_path, :class => 'connect_class')
+    end
+  end
+end
--- app/helpers/my_account/social_helper.rb
+++
@@ -1,2 +0,0 @@
-module MyAccount::SocialHelper
-end
