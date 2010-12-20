87a067f5c7a98d10df3608d041ae444779e2d1cd
Jorge Pardiñas <jorge.parbros@crowdint.com>
2010-12-20 05:18:29 -0600

    personalized url

--- app/controllers/my_account/profile_controller.rb
+++ app/controllers/my_account/profile_controller.rb
@@ -1,7 +1,17 @@
 class MyAccount::ProfileController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => :search
   
   def show
     
+  end
+  
+  def search
+    user = User.find_by_username(params[:user_name])
+    if user
+      event = user.events.active.last
+      redirect_to event_path(event)
+    else
+      redirect_to :root
+    end
   end
 end
