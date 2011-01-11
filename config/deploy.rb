44c1b5e14cfba1b9dcb313d186b57234d4e68919
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-01-11 00:24:21 -0600

    View event button changed for a link

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,24 @@
-set :application, "Gifty Fifty"
-set :repository,  "/home/giftyuser/giftyfifty"
+set :application, "dev.giftyfifty.com:65000"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
+
+set :user, "giftyuser"
+set :deploy_to, "/home/giftyuser/giftyfifty"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "ngnix"                          # Your HTTP server, Apache/etc
-role :app, "thin"                          # This may be the same as your `Web` server
-role :db,  "localhost:3600", :primary => true # This is where Rails migrations will run
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :repository,  "git@github.com:giftyfifty/giftyfifty.git"
+set :branch, "master"
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    invoke_command "cd #{deploy_to}"
+    invoke_command "thin restart -C config/thin.yml"
   end
 end
         require 'config/boot'
