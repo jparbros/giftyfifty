9aa3ec99ea699b9c45d080d44d40ed5b5b172b92
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-13 08:11:24 -0500

    try new config tu upload to google storage

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,5 +4,6 @@ CarrierWave.configure do |config|
     :google_storage_access_key_id     => 'GOOGVZIMJX5HY5U52MDH',
     :google_storage_secret_access_key => 'vntfgFHrds1XeXXXmFPCJYsqw2er7HEtfHtDRi3C'
   }
+  config.storage :fog
   config.fog_directory = 'gifty'
 end
