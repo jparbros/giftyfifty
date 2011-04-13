72bb1ac9d9bceb811c1f5937ee75f4daf4c617ae
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-13 09:10:10 -0500

    SMTP configuration

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :address              => "smtp.sendgrid.net",
+  :port                 => 25,
   :domain               => 'giftyfifty.com',
-  :user_name            => 'sendgrid@giftyfifty.com',
-  :password             => 'Sendgrid1',
+  :user_name            => 'jorge@giftyfifty.com',
+  :password             => '271185',
   :authentication       => 'plain',
-  :enable_starttls_auto => true  
 }
