a348a7dc0ff8e7f0f341d45b177fe085eecc1a09
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-16 21:34:47 -0500

    creating oauth account better

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,10 +100,11 @@ class User < ActiveRecord::Base
   def add_provider(provider, provider_info)
     case provider
       when 'twitter'
-        self.twitter_account.create({:token => provider_info['credentials']['token'], :secret =>provider_info['credentials']['secret']})
+        debugger
+        self.twitter_account = TwitterAccount.create({:token => provider_info['credentials']['token'], :secret =>provider_info['credentials']['secret']})
         get_avatar(provider_info['extra']['user_hash']['profile_image_url'])
       when 'facebook'
-        self.facebook_account.create({ :token => provider_info['credentials']['token']})
+        self.facebook_account = FacebookAccount.create({ :token => provider_info['credentials']['token']})
         get_facebook_avatar
     end
   end
