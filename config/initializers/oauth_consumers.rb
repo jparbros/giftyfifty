1e1ff5c20efdbaca3c1f3202ccad1e09ffed5744
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-22 17:11:02 -0500

    facebook proxy

--- config/initializers/oauth_consumers.rb
+++ config/initializers/oauth_consumers.rb
@@ -7,5 +7,5 @@ TWITTER_CREDENTIALS = {
 FACEBOOK_CREDENTIAL = {
   :client_id => '9265497607cecc1f3ee4877400859b89', 
   :secret_id => '46c0ad920327791c06c2cd6713ea98c9',
-  :facebook_uri => 'http://fb-giftyfifty.apigee.com'
+  :proxy => 'http://fb-giftyfifty.apigee.com'
 }
