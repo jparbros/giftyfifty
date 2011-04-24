655aae05978116e73148310bf3ab4150d240a271
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-24 01:09:54 -0500

    using https to connect with fbgraph

--- config/initializers/oauth_consumers.rb
+++ config/initializers/oauth_consumers.rb
@@ -1,11 +1,11 @@
 TWITTER_CREDENTIALS = {
   :consumer_key => 'xJazt51TQ2ZGwUa6LIwcQ',
   :consumer_secret => 'Wf3vKm7yYYD4VUKJHoWKUEUIepu68vVdUTOo96IB4mE',
-  :proxy => 'http://tw-giftyfifty.apigee.com'
+  :proxy => 'https://tw-giftyfifty.apigee.com'
 }
 
 FACEBOOK_CREDENTIAL = {
   :client_id => '9265497607cecc1f3ee4877400859b89', 
   :secret_id => '46c0ad920327791c06c2cd6713ea98c9',
-  :facebook_uri => 'http://fb-giftyfifty.apigee.com'
+  :facebook_uri => 'https://fb-giftyfifty.apigee.com'
 }
