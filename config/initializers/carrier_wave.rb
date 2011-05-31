439f64c204d9ef6c46e7bdd3323c2e7ec6d4f704
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-05-31 18:19:21 -0500

    Using AWS in carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider                         => 'Google',
-    :google_storage_access_key_id     => ENV['GOOGLE_STORAGE_KEY_ID'],
-    :google_storage_secret_access_key => ENV['GOOGLE_STORAGE_SECRET_ID']
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_ACCESS']  
   }
   config.storage :fog
-  config.fog_directory = ENV['GOOGLE_STORAGE_DIRECTORY']
+  config.fog_directory = ENV['S3_BUCKET']
 end
